6ff8dcfbbdd9e008726ac3b2cd97c607c151c85d
Desiree Quade <nekodez@gmail.com>
2023-03-31 21:09:54 -0500

    fixes

--- database.sql
+++ database.sql
@@ -4,15 +4,15 @@ CREATE TABLE "users" (
     "last_name" VARCHAR (255) NOT NULL,
     "email" VARCHAR (255) UNIQUE NOT NULL,
     "password" VARCHAR (1024) NOT NULL,
-	"profile_image" VARCHAR(1042) DEFAULT 'images/profile-image.jpeg'
+	"profile_image" VARCHAR(1042) DEFAULT 'images/profile-image.jpeg',
     "authLevel" VARCHAR (255) DEFAULT 'USER'
 );
 
-CREATE TABLE "catagories" (
+CREATE TABLE "categories" (
     "id" SERIAL PRIMARY KEY,
     "user_id" INTEGER REFERENCES "users"(id) ON DELETE CASCADE,
     "catagory_name" VARCHAR (255) NOT NULL,
-    "type" ENUM ('BILL', 'GOAL') NOT NULL,
+    "goal" BOOLEAN NOT NULL,
     "amount" INTEGER,
     "date" DATE DEFAULT CURRENT_DATE,
 );
